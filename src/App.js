c05ecd98afca57c0be08c6fa476aa2831425f01e
Alexander Sherwin <alx.sherwin@gmail.com>
2022-05-16 20:57:57 -0600

    first practice with state - warm up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-
+import { useState } from 'react';
 import './App.css';
 
+
 function App() {
+
+  const [count, setCount] = useState(0);
+
   return (
     <div className="App">
-
+      <h1>Hello! The number is {count} </h1>
+      <button onClick={() => setCount( count + 1)}> INCREASE THE NUMBER!!!</button>
+      <button onClick={() => setCount(count - 1)}> DECREASE THE NUMBER!!!</button>
     </div>
   );
 }
