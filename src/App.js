284ef7d6440c8d5c9b3f7a7ab41ce5bacb93e402
Alexander Sherwin <alx.sherwin@gmail.com>
2022-05-16 21:49:25 -0600

    added a random number button

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
+import { decrement, increment, incrementByAmount } from './redux/counter';
 
 
 function App() {
 
   const { count } = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
 
   return (
     <div className="App">
       <h1>The Count is set to</h1>
       <h2>{count}</h2>
       <br/>
-      <button> Increase the Count! </button>
-      <button> Decrease the Count! </button>
+      <button onClick={() => dispatch(increment())}> Increase the Count! </button>
+      <button onClick={() => dispatch(decrement())}> Decrease the Count! </button>
+      <button onClick={() => dispatch(incrementByAmount(Math.random()*10))}> Increase by Random! </button>
 
     </div>
   );
