d76d644be4ef81272bd4a1c350df00d303cd6d6e
Alexander Sherwin <alx.sherwin@gmail.com>
2022-05-16 21:41:20 -0600

    got count to display redux state - wahoo

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import {useState} from 'react'
+
+import { useSelector } from 'react-redux';
 import './App.css';
 
 
 function App() {
 
-  const [counter, setCounter] = useState(0)
+  const { count } = useSelector((state) => state.counter);
 
   return (
     <div className="App">
       <h1>The Count is set to</h1>
-      <h2>{counter}</h2>
+      <h2>{count}</h2>
       <br/>
-      <button onClick={() => setCounter( counter + 1)}> Increase the Count! </button>
-      <button onClick={() => setCounter( counter - 1)}> Decrease the Count! </button>
+      <button> Increase the Count! </button>
+      <button> Decrease the Count! </button>
 
     </div>
   );
